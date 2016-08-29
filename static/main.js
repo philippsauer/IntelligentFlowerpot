732943670fbb767b7ded9a41436d8bf41bc2070b
Tim <mail@tim5chneider.de>
2016-08-29 15:52:28 +0200

    Temperatur Chart eingefügt

--- static/main.js
+++ static/main.js
@@ -1,8 +1,65 @@
-$( document ).ready(function() {
+// Parse local CSV file
+function parseData(createTempGraph) {
+	Papa.parse("../data.csv", {
+		download: true,
+		complete: function(results) {
+			createTempGraph(results.data);
+		}
+	});
+}
+
+function createTempGraph(data) {
+	var date = [];
+	var temp = [];
+	var humidity = [];
+	var brigthness = [];
+	var level = [];
+	
+	for (var i = 0; i < data.length; i++) {
+		date.push(data[i][0]);
+		temp.push(data[i][1]);
+		humidity.push(data[i][2]);
+		brigthness.push(data[i][3]);
+		level.push(data[i][4]);
+	}
+	
+	console.log(date);
+	console.log(temp);
+	console.log(humidity);
+	console.log(brigthness);
+	console.log(level);
+	
+	var ctx = document.getElementById("beleuchtungsChart");
+	var tempChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+				
+          labels: date,
+          datasets: [{
+              data: temp
+          }]
+					
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+	
+}
+
+parseData(createTempGraph);
 
 
+/*$( document ).ready(function() {
+	
   var ctx = document.getElementById("beleuchtungsChart");
-  var myChart = new Chart(ctx, {
+  var tempChart = new Chart(ctx, {
       type: 'line',
       data: {
           labels: ["28.06.2016", "29.06.2016", "30.06.2016", "01.07.2016", "02.07.2016", "03.07.2016"],
@@ -20,4 +77,4 @@ $( document ).ready(function() {
           }
       }
   });
-});
+});*/
