0ee341d4639455c43b599b663f2ccee77284e7bc
Tim <mail@tim5chneider.de>
2016-08-29 17:37:31 +0200

    Added MulipleChart

--- static/main.js
+++ static/main.js
@@ -29,14 +29,32 @@ function createGraph(data) {
 	console.log(brigthness);
 	console.log(level);
 	
-	var ctx = document.getElementById("beleuchtungsChart");
-	var tempChart = new Chart(ctx, {
+	var ctx_temp = document.getElementById("beleuchtungsChart");
+	var tempChart = new Chart(ctx_temp, {
       type: 'line',
       data: {
 				
           labels: date,
           datasets: [{
-              data: temp
+						label: "Temperatur",
+            fill: false,
+						lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: temp
           }]
 					
       },
@@ -50,7 +68,116 @@ function createGraph(data) {
           }
       }
   });
-	
+	var ctx_multiple = document.getElementById("multipleChart").getContext("2d");
+	ctx_multiple.canvas.width = 600;
+	ctx_multiple.canvas.height = 600;
+	var multiplelChart = new Chart(ctx_multiple, {
+      type: 'line',
+      data: {
+				
+          labels: date,
+          datasets: [
+						{
+							label: "Temperatur",
+							fill: false,
+							lineTension: 0.1,
+							backgroundColor: "rgba(255,0,0,0.4)",
+							borderColor: "rgba(255,0,0,1)",
+							borderCapStyle: 'butt',
+							borderDash: [],
+							borderDashOffset: 0.0,
+							borderJoinStyle: 'miter',
+							pointBorderColor: "rgba(255,0,0,1)",
+							pointBackgroundColor: "#fff",
+							pointBorderWidth: 1,
+							pointHoverRadius: 5,
+							pointHoverBackgroundColor: "rgba(255,0,0,1)",
+							pointHoverBorderColor: "rgba(220,220,220,1)",
+							pointHoverBorderWidth: 2,
+							pointRadius: 1,
+							pointHitRadius: 10,
+							data: temp
+						},
+						{
+							label: "Luftfeuchtigkeit",
+							fill: false,
+							backgroundColor: "rgba(74,215,0,0.4)",
+							borderColor: "rgba(74,215,0,1)",
+							borderCapStyle: 'butt',
+							borderDash: [],
+							borderDashOffset: 0.0,
+							borderJoinStyle: 'miter',
+							pointBorderColor: "rgba(74,215,0,1)",
+							pointBackgroundColor: "#fff",
+							pointBorderWidth: 1,
+							pointHoverRadius: 5,
+							pointHoverBackgroundColor: "rgba(74,215,0,1)",
+							pointHoverBorderColor: "rgba(220,220,220,1)",
+							pointHoverBorderWidth: 2,
+							pointRadius: 1,
+							pointHitRadius: 10,
+							data: humidity
+						},
+						{	
+							label: "Helligkeit",
+							fill: false,
+							backgroundColor: "rgba(255,255,0,0.4)",
+							borderColor: "rgba(255,255,0,1)",
+							borderCapStyle: 'butt',
+							borderDash: [],
+							borderDashOffset: 0.0,
+							borderJoinStyle: 'miter',
+							pointBorderColor: "rgba(255,255,0,1)",
+							pointBackgroundColor: "#fff",
+							pointBorderWidth: 1,
+							pointHoverRadius: 5,
+							pointHoverBackgroundColor: "rgba(255,255,0,1)",
+							pointHoverBorderColor: "rgba(220,220,220,1)",
+							pointHoverBorderWidth: 2,
+							pointRadius: 1,
+							pointHitRadius: 10,
+							data: brigthness		
+						},
+						{
+							label: "Füllstand Regentonne",
+							fill: false,
+							lineTension: 0.1,
+							backgroundColor: "rgba(75,192,192,0.4)",
+							borderColor: "rgba(75,192,192,1)",
+							borderCapStyle: 'butt',
+							borderDash: [],
+							borderDashOffset: 0.0,
+							borderJoinStyle: 'miter',
+							pointBorderColor: "rgba(75,192,192,1)",
+							pointBackgroundColor: "#fff",
+							pointBorderWidth: 1,
+							pointHoverRadius: 5,
+							pointHoverBackgroundColor: "rgba(75,192,192,1)",
+							pointHoverBorderColor: "rgba(220,220,220,1)",
+							pointHoverBorderWidth: 2,
+							pointRadius: 1,
+							pointHitRadius: 10,
+							data: level				
+						}
+					]
+					
+      },
+      options: {
+				maintainAspectRatio: true,
+				responsive: true,
+				title:{
+						display:true,
+						text:'Intelligenter Blumentopf'
+				},
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+      }
+  });
 }
 
 parseData(createGraph);
