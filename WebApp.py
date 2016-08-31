4dcfea107330dd39e829008670b91f4e610dca9f
Tim <mail@tim5chneider.de>
2016-08-31 15:00:50 +0200

    Add Charts to Website (parse data.csv direct in head.html)

--- WebApp.py
+++ WebApp.py
@@ -2,9 +2,9 @@ import time
 import config
 import logging
 import threading
-#import csv
+import csv
 
-from flask import Flask, url_for, render_template, request, jsonify
+from flask import Flask, url_for, render_template, request, jsonify #, Response
 from flask.ext.classy import FlaskView, route
 
 app = Flask(__name__)
@@ -40,19 +40,25 @@ class WebApp(FlaskView, threading.Thread):
         app.debug = config.app['debug']  
         app.run(host=host, port=port)
 
-        
+def get_csv():
+    csv_path = 'data.csv'
+    csv_file = open(csv_path, 'rb')
+    csv_obj = csv.reader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
 @app.route("/")
 @app.route("/home")
 @app.route("/index")
 def index():
     template = 'head.html'
-    return render_template(template)
-
-@app.route("/data.csv")
-def data():
-    output = "data.csv"
-#    csv_path = './static/data.csv'
-#    csv_file = open(csv_path, 'rb')
-#    csv_obj = csv.DictReader(csv_file)
-#    output = csv_obj
-    return output
+#    return render_template(template)
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
+    
+#@app.route('/data.csv')
+#def generate_large_csv():
+#    def generate():
+#        for row in iter_all_rows():
+#            yield ','.join(row) + '\n'
+#    return Response(generate(), mimetype='text/csv')
--- static/main.js
+++ static/main.js
@@ -306,16 +306,27 @@ function createGraph(data) {
 
 parseData(createGraph);
 
+/*
 
-/*$( document ).ready(function() {
+$( document ).ready(function() {
 	
-  var ctx = document.getElementById("beleuchtungsChart");
-  var tempChart = new Chart(ctx, {
+	var ctx_temp = document.getElementById("temperature");
+	var tempChart = new Chart(ctx_temp, {
       type: 'line',
       data: {
-          labels: ["28.06.2016", "29.06.2016", "30.06.2016", "01.07.2016", "02.07.2016", "03.07.2016"],
+          labels: [
+						{% for obj in object_list %}
+						"{{ obj[0] }}"
+						{% if not loop.last %},{% endif %}
+						{% endfor %}
+					],
           datasets: [{
-              data: [12, 19, 3, 5, 2, 3]
+              data: [
+								{% for obj in object_list %}
+								{{ obj[1] }}
+								{% if not loop.last %},{% endif %}
+								{% endfor %}
+							]
           }]
       },
       options: {
@@ -328,4 +339,5 @@ parseData(createGraph);
           }
       }
   });
-});*/
+});
+*/
